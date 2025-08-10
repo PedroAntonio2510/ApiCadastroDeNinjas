12a57b81184baec832bfe91b708fbcb374a3f56f
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-08-10 16:05:12 -0300

    feat: coloca no MissoesRepository o JpaRepository

---
+++ src/main/java/dev/java10x/cadastroninjas/Missao/MissoesRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.cadastroninjas.Missao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissaoModel, Long> {
+}
