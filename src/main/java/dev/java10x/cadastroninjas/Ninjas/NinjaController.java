82f86922e3de771f0e85b654aa899b57bece6f95
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-08-14 15:10:30 -0300

    feat: Adiciona endpoint dos ninjas

--- src/main/java/dev/java10x/cadastroninjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/cadastroninjas/Ninjas/NinjaController.java
@@ -1,7 +1,6 @@
 package dev.java10x.cadastroninjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class NinjaController {
@@ -10,4 +9,35 @@ public class NinjaController {
     public String boasVindas() {
         return "Essa e a minha mensagem nessa rota";
     }
+
+    //Adicionar Ninja(CREATE)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    //Mostrar todos os ninjas(READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas() {
+        return "Mostrar Ninja";
+    }
+
+    //Mostrar Ninja por ID(READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPorId() {
+        return "Mostrar Ninja por ID";
+    }
+
+    //Alterar dados dos ninjas(UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId(){
+        return "Alterar Ninja por id";
+    }
+
+    // Deletar Ninja(DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId() {
+        return "Deletar Ninja por id";
+    }
+
 }
