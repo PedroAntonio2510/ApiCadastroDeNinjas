0dc24b04c5ee9cf418d67a90dccdc756b99267ba
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-08-15 17:07:59 -0300

    feat: cria os endpoints de criar, findById, e listar utilizando POST e GET

--- src/main/java/dev/java10x/cadastroninjas/Missao/MissaoController.java
+++ src/main/java/dev/java10x/cadastroninjas/Missao/MissaoController.java
@@ -2,18 +2,31 @@ package dev.java10x.cadastroninjas.Missao;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("missoes")
 public class MissaoController {
 
+    private MissaoService missaoService;
+
+    public MissaoController(MissaoService missaoService) {
+        this.missaoService = missaoService;
+    }
+
     @GetMapping("/listar")
-    public String listarMissoes() {
-        return "Missoes listadas";
+    public List<MissaoModel> listarMissoes() {
+        return missaoService.listarMissoes();
+    }
+
+    @GetMapping("/listar/{id}")
+    public MissaoModel listarMissaoPorId(@PathVariable Long id) {
+        return missaoService.listarMissaoPorId(id);
     }
 
     @PostMapping("/criar")
-    public String criarMissao() {
-        return "Missao criada com sucesso";
+    public MissaoModel criarMissao(@RequestBody MissaoModel missao) {
+        return missaoService.criarMissao(missao);
     }
 
     @PutMapping("/alterarID")
---
+++ src/main/java/dev/java10x/cadastroninjas/Missao/MissaoService.java
@@ -0,0 +1,35 @@
+package dev.java10x.cadastroninjas.Missao;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MissaoService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissaoService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    // Cria a missao
+    public MissaoModel criarMissao(MissaoModel missao) {
+        return missoesRepository.save(missao);
+    }
+
+    // Listar missoes
+    public List<MissaoModel> listarMissoes() {
+        return missoesRepository.findAll();
+    }
+
+    // Lista missao por id
+    public MissaoModel listarMissaoPorId(Long id) {
+        Optional<MissaoModel> missaoModel = missoesRepository.findById(id);
+        return missaoModel.orElse(null);
+    }
+
+
+}
+
