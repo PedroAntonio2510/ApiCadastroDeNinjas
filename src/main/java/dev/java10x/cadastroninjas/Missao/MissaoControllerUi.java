5c80a559a6a3521d8ae96278bba5f355c6281885
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-08-19 22:51:58 -0300

    feat: Adiciona a tela de listagem de missoes juntamente do MissaoControllerUi

---
+++ src/main/java/dev/java10x/cadastroninjas/Missao/MissaoControllerUi.java
@@ -0,0 +1,27 @@
+package dev.java10x.cadastroninjas.Missao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/missoes/ui")
+public class MissaoControllerUi {
+
+        private final MissaoService missaoService;
+
+        public MissaoControllerUi(MissaoService missaoService) {
+            this.missaoService = missaoService;
+        }
+
+        @GetMapping("/listar")
+        public String listarMissoes(Model model) {
+            List<MissaoDTO> missoes = missaoService.listarMissoes();
+            model.addAttribute("missoes", missoes);
+            return "listarMissoes";
+        }
+    
+}
